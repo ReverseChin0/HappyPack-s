7beefcfaf273326a24191fab2a5bf484b55b3390
ReverseChin0 <38972126+ReverseChin0@users.noreply.github.com>
2020-05-31 15:28:45 -0500

    MenuPrincipalSencishito

---
+++ Assets/_Proyecto/Scripts/UI/SCR_PressAnyButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_PressAnyButton : MonoBehaviour
+{
+    public GameObject activable;
+    private void Update() {
+        if (Input.anyKey) {
+            activable.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+        if (Input.touchCount > 0) {
+            activable.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/_Proyecto/Scripts/UI/SCR_UITweener.cs
+++ Assets/_Proyecto/Scripts/UI/SCR_UITweener.cs
@@ -3,34 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+/*
+ * Francisco Emmanuel Castañeda López
+ * Libreria de Tween para UI Extensible
+ */
+
 public enum TipoDeTween
 {
     Mover,
-    Escalar
+    Escalar,
+    Desvanecer,
+    Saltar,
+    Columpiar
 }
 
 public class SCR_UITweener : MonoBehaviour
 {
     [SerializeField] private bool reproducirStart = false;
     [SerializeField] private bool reproducirEnable = false;
-    [SerializeField] private bool posicionInicial = false;
+    [SerializeField] private bool usarPosicionInicial = false;
+    [SerializeField] private bool loops = false;
     [SerializeField] private float delay = 0.0f, duracion = 0.5f;
     [SerializeField] private TipoDeTween tipoTween = TipoDeTween.Mover;
     [SerializeField] private Ease tipoDeSuavizado = default;
     [SerializeField] private Vector2 posfinal=default;
     [SerializeField] Vector2 posInicial=default;
-    RectTransform rectTransform;
-    Sequence mySequence;
+    RectTransform rectTransform = default;
+    CanvasGroup cGroup = default;
 
     private void Awake() 
     {
         rectTransform = GetComponent<RectTransform>();
-        mySequence = DOTween.Sequence();
+
+        if (tipoTween == TipoDeTween.Desvanecer) {
+            cGroup = GetComponent<CanvasGroup>();
+            if (cGroup == null)
+                cGroup = gameObject.AddComponent<CanvasGroup>();
+        }
     }
 
     private void Start() 
     {
-        if (!posicionInicial)
+        if (!usarPosicionInicial)
             posInicial = rectTransform.anchoredPosition;
 
         if (reproducirStart)
@@ -48,29 +62,60 @@ public class SCR_UITweener : MonoBehaviour
         switch (tipoTween) 
         {
             case TipoDeTween.Mover:
-                if (posicionInicial)
-                    rectTransform.anchoredPosition = new Vector3(posInicial.x, posInicial.y, 0);
+                if (usarPosicionInicial)
+                    rectTransform.anchoredPosition = posInicial;
 
                 rectTransform.
                     DOAnchorPos(posfinal, duracion).
                     SetDelay(delay).
-                    SetEase(tipoDeSuavizado);
+                    SetEase(tipoDeSuavizado).SetLoops(loops ? -1 : 0);
                 break;
 
             case TipoDeTween.Escalar:
-                if (posicionInicial)
+                if (usarPosicionInicial)
                     rectTransform.localScale = new Vector3(posInicial.x, posInicial.y, 0);
 
                 rectTransform.
                     DOScale(new Vector3(posfinal.x,posfinal.y,0),duracion).
                     SetDelay(delay).
-                    SetEase(tipoDeSuavizado);
+                    SetEase(tipoDeSuavizado).
+                    SetLoops(loops ? -1:0);
                 break;
 
-            default: break;
-        }
+            case TipoDeTween.Desvanecer:
+                if (usarPosicionInicial)
+                    cGroup.alpha = posInicial.x;
 
-        
+                cGroup.
+                    DOFade(posfinal.x,duracion).
+                    SetDelay(delay).
+                    SetEase(tipoDeSuavizado).
+                    SetLoops(loops ? -1 : 0,LoopType.Yoyo);
+                break;
+
+            case TipoDeTween.Saltar:
+                if (usarPosicionInicial)
+                    rectTransform.anchoredPosition = posInicial;
+
+                rectTransform.
+                    DOJumpAnchorPos(posfinal,100.0f,0,duracion).
+                    SetDelay(delay).
+                    SetLoops(loops ? -1 : 0);
+                break;
+
+            case TipoDeTween.Columpiar:
+                if (usarPosicionInicial)
+                    rectTransform.rotation = Quaternion.Euler(new Vector3(0, 0, posInicial.x));
+
+                rectTransform.
+                    DOLocalRotate(new Vector3(0,0,posfinal.x),duracion).
+                    SetEase(tipoDeSuavizado).
+                    SetLoops(loops ? -1 : 0,LoopType.Yoyo);
+                break;
+
+            default:
+                break;
+        }
     }
 
     public void Switch() 
@@ -78,5 +123,10 @@ public class SCR_UITweener : MonoBehaviour
         Vector2 temp = posInicial;
         posInicial = posfinal;
         posfinal = temp;
+    }
+
+    public void InvertirYReproducir() {
+        Switch();
+        Reproducir();
     }
 }
