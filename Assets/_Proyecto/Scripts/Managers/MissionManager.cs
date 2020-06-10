106eb182da58db0e329658b8c33e94c8c111f489
ReverseChin0 <38972126+ReverseChin0@users.noreply.github.com>
2020-06-10 09:12:56 -0500

    shader

---
+++ Assets/_Proyecto/Scripts/Managers/MissionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour
+{
+    [SerializeField] Transform[] puntosInicio;
+    [SerializeField] GameObject BeginPref = default, EndPref = default;
+    bool[] ocupados;
+    public int maxMisiones = 3;
+
+    private void Start() 
+    {
+        ocupados = new bool[puntosInicio.Length];
+
+        for (int o = 0; o < ocupados.Length; o++) 
+            ocupados[o] = false;
+        
+        for (int i = 0; i < maxMisiones; i++) {
+            int selector = Random.Range(0, puntosInicio.Length);
+
+            while(ocupados[selector]==true)
+                selector = Random.Range(0, puntosInicio.Length);
+
+            ocupados[selector] = true;
+            Instantiate(BeginPref, puntosInicio[selector].position, Quaternion.identity, puntosInicio[selector].parent);
+        }
+    }
+
+}
---
+++ Assets/_Proyecto/Scripts/ShaderProperties/ShaderColor.cs
@@ -0,0 +1,39 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderColor : MonoBehaviour
+{
+    [SerializeField, ColorUsage(true, true)] Color color;
+    [SerializeField] string colorString = "_color", floatOnTrigger = "_alphaVal";
+    [SerializeField] float desde = 1.0f, hasta = 0.0f , duracion = 1.0f;
+    float alpha = 0;
+    Renderer miRenderer;
+    MaterialPropertyBlock miProp;
+
+    private void Awake() 
+    {
+        miRenderer = GetComponent<Renderer>();
+        miProp = new MaterialPropertyBlock();
+    }
+
+    private void Start() 
+    {
+        miProp.SetColor(colorString, color);
+        miRenderer.SetPropertyBlock(miProp);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Player")) {
+            DOTween.To(UpdatePropBlock,desde,hasta, duracion).SetEase(Ease.OutExpo);
+        }
+    }
+
+    void UpdatePropBlock(float aplha) {
+        print(aplha);
+        miProp.SetFloat(floatOnTrigger, aplha);
+        miRenderer.SetPropertyBlock(miProp);
+    }
+}
