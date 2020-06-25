c17dd3bd14315745331271c1f8fa4e2fafe0ce69
ReverseChin0 <38972126+ReverseChin0@users.noreply.github.com>
2020-06-25 09:49:11 -0500

    scriptsGuardado

--- Assets/_Proyecto/Scripts/Managers/SCR_PlayerProgress.cs
+++ Assets/_Proyecto/Scripts/Managers/SCR_PlayerProgress.cs
@@ -15,6 +15,8 @@ public class SCR_PlayerProgress : MonoBehaviour
 
     private void Awake() 
     {
+        //==============================================================================================================================================||
+
         if (PlayerPrefs.HasKey("dinero")) //checo si ya hay llave que guarde el dinero
             dinerActual = PlayerPrefs.GetInt("dinero"); //si si le pido el dinero
         else 
@@ -34,22 +36,24 @@ public class SCR_PlayerProgress : MonoBehaviour
             txt_misioHistoria.text =  misiActual.ToString(); //asigno dineroactual
 
         //==============================================================================================================================================||
-        
     }
 
+    [ContextMenu("saveStats")]
     public void SaveStats() 
     {
         PlayerPrefs.SetInt("dinero", dinerActual);
         PlayerPrefs.SetInt("misiones", misiActual);
         saveSequence = DOTween.Sequence();
-        saveSequence.Append(saveIcon.DOFade(1, 0.25f))
-                    .Append(saveIcon.DOFade(.1f, 0.25f))
-                    .Append(saveIcon.DOFade(1, 0.25f))
-                    .Append(saveIcon.DOFade(.1f, 0.25f));
+        saveSequence.Append(saveIcon.DOFade(1, 0.35f))
+                    .Append(saveIcon.DOFade(.1f, 0.1f))
+                    .Append(saveIcon.DOFade(1, 0.35f))
+                    .Append(saveIcon.DOFade(0f, 0.1f));
+        //saveSequence.Play();
     }
 
     [ContextMenu("resetStats")]
-    public void ResetStats() { //reiniciar stats
+    public void ResetStats() 
+    { //reiniciar stats
         PlayerPrefs.SetInt("dinero",0);
         PlayerPrefs.SetInt("misiones",0);
         dinerActual = 0;
@@ -57,5 +61,4 @@ public class SCR_PlayerProgress : MonoBehaviour
         txt_dinero.text = "$" + dinerActual.ToString(); //asigno dineroactual
         txt_misioHistoria.text = misiActual.ToString(); //asigno dineroactual
     }
-
 }
